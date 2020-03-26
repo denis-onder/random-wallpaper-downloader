a9b658e0964f866c5e2cd0d01eb2a03f80dcd494
Denis Onder <denis_onder@protonmail.com>
2020-03-26 19:45:21 +0100

    Added some flair

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const fs = require("fs");
-const request = require("request");
+const fs = require("fs"),
+  request = require("request"),
+  figlet = require("figlet");
 
-const URL = "https://source.unsplash.com/random/3840x2160/?wallpaper";
+// Load environmental variables
+require("dotenv").config();
 
-(() => request(URL).pipe(fs.createWriteStream(`${Date.now()}.jpeg`)))();
+// Greet user with figlet
+figlet("Random\nWallpaper\nDownloader", (_, res) =>
+  console.log(`${res}\nPowered by the Unsplash API!\n`)
+);
+
+const URL = "https://source.unsplash.com/random/3840x2160/?wallpaper",
+  NAME = `${Date.now()}.jpeg`,
+  DIRECTORY = `${process.env.HOME}${process.env.IMG_DIR}/${NAME}`;
+
+request(URL)
+  .pipe(fs.createWriteStream(NAME))
+  .on("close", () => {
+    fs.rename(NAME, DIRECTORY, () =>
+      console.log(`Acquired new wallpaper!\n${DIRECTORY}`)
+    );
+  });
